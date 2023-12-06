897bf292e47f1b4496be7180f9f565ba8ddba277
kamaljit67802 <133055105+kamaljit67802@users.noreply.github.com>
2023-12-06 08:08:35 +0530

    new models
    Added missing model and modification in model.

--- Mynotes/Models/User.cs
+++ Mynotes/Models/User.cs
@@ -1,3 +1,5 @@
+// Purpose: Model for the User table in the database.
+// since identityUser already includes properties for Id, PasswordHash, and Email. No need to add it separately.
 using Microsoft.AspNetCore.Identity;
 
 namespace Mynotes.Models
