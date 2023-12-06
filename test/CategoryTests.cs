63fcf559fc342b4608d9ec5ccc38005e4f4d0b58
Kamaljit Kaur <kamaljit67802@gmail.com>
2023-12-06 08:43:31 +0530

    Add CategoryTests

---
+++ test/CategoryTests.cs
@@ -0,0 +1,17 @@
+// Tests/CategoryTests.cs
+using Xunit;
+using NotesApp.Models;
+
+public class CategoryTests
+{
+    [Fact]
+    public void CanCreateCategory()
+    {
+        var category = new Category
+        {
+            CategoryName = "Test Category"
+        };
+
+        Assert.Equal("Test Category", category.CategoryName);
+    }
+}
