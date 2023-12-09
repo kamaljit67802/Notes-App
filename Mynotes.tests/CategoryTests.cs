faeb44b9f472e50e13dc7faadbd88b7ac6ab89f4
Kamaljit Kaur <kamaljit67802@gmail.com>
2023-12-09 08:52:13 +0530

    Changes

--- Mynotes.tests/CategoryTests.cs
+++ Mynotes.tests/CategoryTests.cs
@@ -8,13 +8,13 @@ public class CategoryTests
     [Fact]
     public void CanChangeCategoryName()
     {
-        // Arrange
+        
         var category = new Category { Name = "Test Category" };
 
-        // Act
+        
         category.Name = "New Category";
 
-        // Assert
+        
         Assert.Equal("New Category", category.Name);
     }
 }
--- Mynotes.tests/UserTests.cs
+++ Mynotes.tests/UserTests.cs
@@ -8,26 +8,26 @@ namespace Mynotes.Tests
         [Fact]
         public void CanChangeFirstName()
         {
-            // set user to test
+            
             var user = new User { FirstName = "Test" };
 
-            // Act
+            
             user.FirstName = "John Doe";
 
-            // Assert
+            
             Assert.Equal("John Doe", user.FirstName);
         }
 
         [Fact]
         public void CanChangeLastName()
         {
-            // set
+            
             var user = new User { LastName = "Test" };
 
-            // fire
+
             user.LastName = "New Name";
 
-            // check
+    
             Assert.Equal("New Name", user.LastName);
         }
     }
