84fec5f6c134bce3c6ea5f3a356f280765508b10
Kamaljit Kaur <kamaljit67802@gmail.com>
2023-12-11 09:25:33 +0530

    Add changes

--- Mynotes/Controllers/AccountController.cs
+++ Mynotes/Controllers/AccountController.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Identity;
-using Mynotes.Models; 
+using Mynotes.Models;
 using Mynotes.ViewModels;
 using System.Threading.Tasks;
 
@@ -18,16 +18,19 @@ namespace Mynotes.Controllers
         }
 
         [HttpGet]
-        public IActionResult Register()
-        {
-            return View(new RegisterViewModel());
-        }
+        public IActionResult Register() => View(new RegisterViewModel());
 
         [HttpPost]
         public async Task<IActionResult> Register(RegisterViewModel model)
         {
             if (ModelState.IsValid)
             {
+                if (string.IsNullOrWhiteSpace(model.Email))
+                {
+                    ModelState.AddModelError(nameof(model.Email), "Email is required.");
+                    return View(model);
+                }
+
                 var user = new User
                 {
                     FirstName = model.FirstName,
@@ -41,33 +44,60 @@ namespace Mynotes.Controllers
                 if (result.Succeeded)
                 {
                     await _signInManager.SignInAsync(user, false);
-                    return RedirectToAction(nameof(Index), "home");
+                    return RedirectToAction(nameof(Index), "Home");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
                 }
             }
 
             return View(model);
         }
 
-        [HttpGet("Login")]
-        public IActionResult Login()
-        {
-            return View(new LoginViewModel());
-        }
+        [HttpGet]
+        public IActionResult Login() => View(new LoginViewModel());
 
         [HttpPost]
         public async Task<IActionResult> Login(LoginViewModel model)
         {
             if (ModelState.IsValid)
             {
+                if (string.IsNullOrWhiteSpace(model.UserName) || string.IsNullOrWhiteSpace(model.Password))
+                {
+                    ModelState.AddModelError(string.Empty, "Username and password are required.");
+                    return View(model);
+                }
+
                 var result = await _signInManager.PasswordSignInAsync(
                     model.UserName,
                     model.Password,
                     model.RememberMe,
-                    false);
+                    lockoutOnFailure: false);
 
                 if (result.Succeeded)
                 {
                     return RedirectToAction(nameof(Index), "Home");
+                }
+
+                if (result.RequiresTwoFactor)
+                {
+                    // Handle two-factor authentication, if needed
+                    // You may add code here if you have two-factor authentication
+                    return RedirectToAction(nameof(Index), "Home"); // Example redirection
+                }
+
+                if (result.IsLockedOut)
+                {
+                    // Handle account lockout, if needed
+                    // You may add code here to handle lockout
+                    return RedirectToAction(nameof(Index), "Home"); // Example redirection
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Invalid login attempt");
+                    return View(model);
                 }
             }
 
