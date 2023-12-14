ebe4494896fcdd5ecd1383b6239547c289f15b3c
Kamaljit Kaur <kamaljit67802@gmail.com>
2023-12-15 03:45:29 +0530

    Add changes

--- Mynotes/Program.cs
+++ Mynotes/Program.cs
@@ -3,7 +3,10 @@ using Microsoft.EntityFrameworkCore;
 using Mynotes.Data;
 using Mynotes.Models;
 using Microsoft.AspNetCore.Builder;
-
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,8 +16,13 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<User>(options =>
+    {
+        options.SignIn.RequireConfirmedAccount = true;
+        
+    })
     .AddEntityFrameworkStores<ApplicationDbContext>();
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
@@ -23,11 +31,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseMigrationsEndPoint();
+    app.UseDeveloperExceptionPage();
 }
 else
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -36,11 +44,11 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
 
 app.Run();
--- Mynotes/ViewModels/LoginViewModel.cs
+++ Mynotes/ViewModels/LoginViewModel.cs
@@ -4,8 +4,12 @@ namespace Mynotes.ViewModels
 {
     public class LoginViewModel
     {
+        [Required(ErrorMessage = "Username is required.")]
         public string? UserName { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
         public string? Password { get; set; }
+
         public bool RememberMe { get; set; }
     }
 }
