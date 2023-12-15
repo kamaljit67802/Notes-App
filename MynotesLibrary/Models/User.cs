b46a1ea138c213f0e4630bbc41b7e7aa15ead379
Kamaljit Kaur <kamaljit67802@gmail.com>
2023-12-16 04:12:04 +0530

    Add changes

--- MynotesLibrary/Models/User.cs
+++ MynotesLibrary/Models/User.cs
@@ -1,7 +1,7 @@
 // Mynotes.Models.User.cs
 
 using Microsoft.AspNetCore.Identity;
-using System.Collections.Generic; // Add this using directive
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace Mynotes.Models
@@ -11,13 +11,13 @@ namespace Mynotes.Models
         [Required(ErrorMessage = "First Name is required")]
         [StringLength(50, ErrorMessage = "First Name cannot exceed 50 characters")]
         [Display(Name = "First Name")]
-        public string? FirstName { get; set; }
+        public string FirstName { get; set; }
 
         [Required(ErrorMessage = "Last Name is required")]
         [StringLength(50, ErrorMessage = "Last Name cannot exceed 50 characters")]
         [Display(Name = "Last Name")]
-        public string? LastName { get; set; }
+        public string LastName { get; set; }
 
-        public List<Note>? Notes { get; set; } // Move this property inside the class
+        public List<Note> Notes { get; set; } = new List<Note>(); // Initialize the list in the constructor
     }
 }
